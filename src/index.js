2d6de46b595c26249c0e7a188dc13f8cb5f71f15
shiv <shivkumarraj333@gmail.com>
2020-04-15 01:17:24 +0530

    hello bahe

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './Login.js';
+ 
+class App extends Comment { 
+  render() {
+    return (
+      <div>
+        <LoginBox />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/> ,document.getElementById('root'));
+ 
+   
+ 
+  
+
+
+
